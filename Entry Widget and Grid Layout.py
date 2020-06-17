03b9133d53200373b3f5df8487be4c1c9c3dfa89
Alex Mercer <alexmercerr07@gmail.com>
2020-06-17 20:26:37 +0530

    Entry widget and grid layouts are explained in this file

---
+++ Entry Widget and Grid Layout.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("655x333")
+root.maxsize(655, 333)
+root.minsize(655, 333)
+root.title("Entry Widget and Grid Layout")
+
+
+
+
+
+
+
+root.mainloop()
