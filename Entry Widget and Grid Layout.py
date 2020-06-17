5771e936c1837cf06db9c96a5fc05715ce9e5306
Alex Mercer <alexmercerr07@gmail.com>
2020-06-17 22:48:02 +0530

    Entry widget and grid layouts are explained in this file

--- Entry Widget and Grid Layout.py
+++ Entry Widget and Grid Layout.py
@@ -6,10 +6,21 @@ root.maxsize(655, 333)
 root.minsize(655, 333)
 root.title("Entry Widget and Grid Layout")
 
+user = Label(text="UserName : ")
+password = Label(text="Password : ")
+user.grid()
+password.grid(row=1)
 
+# Variable classes in tkinter
+# IntVar, BooleanVar, DoubleVar, StringVar
 
+uservalue = StringVar()
+userpass = StringVar()
 
+userentry = Entry(root, textvariable=uservalue)
+passentry = Entry(root, textvariable=userpass)
 
-
+userentry.grid(row=0, column=1)
+passentry.grid(row=1, column=1, padx=180)
 
 root.mainloop()
