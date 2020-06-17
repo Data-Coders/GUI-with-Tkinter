dc9b5ba79b76fb15ac775606089409a3d29368a3
Alex Mercer <alexmercerr07@gmail.com>
2020-06-17 20:33:12 +0530

    Buttons are explained in this file indetail

---
+++ buttons.py
@@ -0,0 +1,16 @@
+from tkinter import *
+root = Tk()
+root.geometry("655x333")
+root.title("Buttons By Code With Harry")
+
+f1 = Frame(root, borderwidth=3)
+f1.pack()
+
+b1 = Button(f1, text="Print Now")
+b1.pack(pady=142)
+
+
+
+
+
+root.mainloop()
