6a8dd930a3229dd067706cd1447770facf366da4
Alex Mercer <alexmercerr07@gmail.com>
2020-06-18 20:52:51 +0530

    How 2 create a Menu in our GUI is showed in this file

---
+++ Menu and Sub Menus.py
@@ -0,0 +1,35 @@
+from tkinter import *
+root = Tk()
+root.geometry("655x333")
+root.minsize(655, 333)
+root.maxsize(655, 333)
+root.title("Menu and Sub Menus in Tkinter")
+
+# non dropdown menu
+# def file():
+#     print('It works')
+# mymenu = Menu(root)
+# mymenu.add_command(label='File', command=file)
+# mymenu.add_command(label='Exit', command=quit)
+# root.config(menu=mymenu)
+
+# Dropdown Menubar
+def hello():
+    print('It works')
+
+mymenu = Menu(root)
+file = Menu(mymenu, tearoff=0)
+file.add_command(label='New Project', command=hello)
+file.add_separator()
+file.add_command(label='New', command=hello)
+file.add_command(label='New Scratch File', command=hello)
+file.add_separator()
+file.add_command(labe='Open', command=hello)
+mymenu.add_cascade(label="File", menu=file)
+root.config(menu=mymenu)
+
+
+
+
+
+root.mainloop()
