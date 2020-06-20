1bc4dd07aef5f4e05f4150b2156d1854c7c57647
Alex Mercer <alexmercerr07@gmail.com>
2020-06-20 23:27:33 +0530

    Entry widget and apcking items with Grid is explianed in this file

--- Entry Widget and Grid Layout.py
+++ Entry Widget and Grid Layout.py
@@ -1,11 +1,16 @@
+# importing nesessary modules in our program
 from tkinter import *
 
+# making root as a function as Tk is
 root = Tk()
+
+# updating title, maximum, minimum, and default window size of our application
 root.geometry("655x333")
 root.maxsize(655, 333)
 root.minsize(655, 333)
 root.title("Entry Widget and Grid Layout")
 
+# creating Labels and packing it with the help of grid
 user = Label(text="UserName : ")
 password = Label(text="Password : ")
 user.grid()
@@ -14,13 +19,17 @@ password.grid(row=1)
 # Variable classes in tkinter
 # IntVar, BooleanVar, DoubleVar, StringVar
 
+# creating two variable to store the data entered through GUI program
 uservalue = StringVar()
 userpass = StringVar()
 
+# creating two Entry where the user will enter its data
 userentry = Entry(root, textvariable=uservalue)
 passentry = Entry(root, textvariable=userpass)
 
+# packing the entry with the help of grid
 userentry.grid(row=0, column=1)
 passentry.grid(row=1, column=1, padx=180)
 
+# making our Application to go in a Event Loop
 root.mainloop()
