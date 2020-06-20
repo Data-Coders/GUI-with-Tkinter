72b4fae99c52867db1323a9e34491b3c072fb799
Alex Mercer <alexmercerr07@gmail.com>
2020-06-20 23:29:48 +0530

    How to create a Button is Explained in this file

--- buttons.py
+++ buttons.py
@@ -1,16 +1,22 @@
+# importing nessesary modules in our program
 from tkinter import *
+
+# making root as a function in our program
 root = Tk()
+
+# setting the default height and width of our Application
 root.geometry("655x333")
+
+# updating the tittle of our Program
 root.title("Buttons By Code With Harry")
 
+# creating a frame and packing it
 f1 = Frame(root, borderwidth=3)
 f1.pack()
 
+# creating a button on the frame we created and packing it with the help of frame we created
 b1 = Button(f1, text="Print Now")
 b1.pack(pady=142)
 
-
-
-
-
+# making our app to go in a event loop
 root.mainloop()
